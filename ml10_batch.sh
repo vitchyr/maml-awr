dae28418585d69b8df7fde5c8bf1206b204dc29a
rmrafailov <43590103+rmrafailov@users.noreply.github.com>
2020-01-31 17:17:24 -0800

    Add files via upload

--- ml10_batch.sh
+++ ml10_batch.sh
@@ -11,4 +11,4 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.0/lib64:/sailhome/rafailov/.mujoco/mujoco200/bin
 source /iris/u/rafailov/cluster_env/cluster_env/bin/activate
 cd /iris/u/rafailov/maml_awr/maml_awr/maml-awr
-python -m run_experiment --env ml10 --mltest --task_idx $SLURM_ARRAY_TASK_ID --full_buffer_size 5000000 --save_dir /home/rafael/IRIS/trained_buffers
+python -m run_experiment --env ml10 --mltest --task_idx $SLURM_ARRAY_TASK_ID --full_buffer_size 75000 --save_dir /iris/u/rafailov/data
