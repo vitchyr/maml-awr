2c0663315097230a0f52d459e83cc055caa72565
Eric Mitchell <em7@iris-ws-3.stanford.edu>
2020-05-06 09:41:20 -0700

    Minor update to macaw_vel script.

--- scripts/macaw_vel.sh
+++ scripts/macaw_vel.sh
@@ -3,11 +3,11 @@
 #SBATCH --time=96:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=96G
+#SBATCH --mem=48G
 #SBATCH --gres=gpu:1
 #SBATCH --job-name="vel"
 
 source env4/bin/activate
 which python
 
-python -m run --name macaw_vel_rollback_args --log_dir log/newruns2 --device cuda:0 --task_config config/cheetah_vel/40tasks_offline.json --macaw_params config/alg/standard.json
+python -m run --name macaw_vel --log_dir log/newruns2 --device cuda:0 --task_config config/cheetah_vel/half_tasks_offline.json --macaw_params config/alg/standard.json
