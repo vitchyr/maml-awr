9cc9b535266b13be2548ef78fd17a31e5cba09a5
Ubuntu <ubuntu@ip-172-31-26-238.us-east-2.compute.internal>
2020-05-15 03:38:34 +0000

    Fixed metaworld version issue, fixed ML45 script.

--- ML45_buffers.sh
+++ ML45_buffers.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # these values are inclusive
-START=0
-END=15
+START=16
+END=25
 
-for i in {$START..$END}
+for i in $(seq $START $END)
 do
-    python -m collect_buffers --env ml45 --task_idx $i --alg sac --replay_buffer_size 1000000 --full_buffer_size 5000000 --task_path /home/ubuntu/maml-awr/tasks --log_dir /home/ubuntu/NIPS2020_data/ML45 &
+    CMD="python -m collect_buffers --env ml45 --task_idx $i --alg sac --replay_buffer_size 1000000 --full_buffer_size 5000000 --task_path /home/ubuntu/maml-awr/tasks --log_dir /home/ubuntu/NIPS2020_data/ML45"
+    echo $CMD
+    $CMD &
 done
