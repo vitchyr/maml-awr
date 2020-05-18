6099420da54843e0970b584c41084d734445cd79
Eric Mitchell <em7@iris-ws-3.stanford.edu>
2020-05-18 16:34:06 -0700

    Tweaked adv_regression scripts for new layer type.

--- scripts/adv_regression/macaw_vel.sh
+++ scripts/adv_regression/macaw_vel.sh
@@ -7,7 +7,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --job-name="vel"
 
-source env4/bin/activate
+eval "$(conda shell.bash hook)"
+conda activate macaw
 which python
 
-python -m run --name macaw_vel_adv --log_dir log/newruns3 --device cuda:0 --task_config config/cheetah_vel/half_tasks_offline.json --macaw_params config/alg/adv_regression.json --buffer_skip 2
+python -m run --name macaw_vel_adv --log_dir log/newruns3 --device cuda:0 --task_config config/cheetah_vel/half_tasks_offline.json --macaw_params config/alg/adv_regression.json --buffer_skip 2 --wlinear
