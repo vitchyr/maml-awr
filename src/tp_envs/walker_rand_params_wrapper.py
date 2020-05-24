9220bf3c6e2557b1b0d5f369849993e6395349d5
jasonpeng142@hotmail.com <jasonpeng142@hotmail.com>
2020-05-24 10:00:57 -0700

    fix

--- src/tp_envs/walker_rand_params_wrapper.py
+++ src/tp_envs/walker_rand_params_wrapper.py
@@ -10,9 +10,7 @@ class WalkerRandParamsWrappedEnv(Walker2DRandParamsEnv):
         super(WalkerRandParamsWrappedEnv, self).__init__()
         self.tasks = self.sample_tasks(n_tasks)
         self.reset_task(0)
-
-    def n_tasks() -> int:
-        return len(self.tasks)
+        self.n_tasks = n_tasks
 
     def get_all_task_idx(self):
         return range(len(self.tasks))
