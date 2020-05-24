d6819e44fad70ef27c8aaa7956ddd4dab997a8aa
jasonpeng142@hotmail.com <jasonpeng142@hotmail.com>
2020-05-23 20:28:35 -0700

    setup

---
+++ scripts/multitask_walker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAME="multitask_walker"
+LOG_DIR="log/mt_test"
+TASK_CONFIG="config/walker_params/50tasks_offline_iris.json"
+MACAW_PARAMS="config/alg/multitask.json"
+
+./scripts/runner.sh $NAME $LOG_DIR $TASK_CONFIG $MACAW_PARAMS
