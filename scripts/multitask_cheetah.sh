774ee51f2152c0c482be0967b6d646208ba80c34
jasonpeng142@hotmail.com <jasonpeng142@hotmail.com>
2020-05-25 22:14:42 -0700

    fix

--- scripts/multitask_cheetah.sh
+++ scripts/multitask_cheetah.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NAME="multitask_cheetah_vel"
+NAME="multitask_cheetah_dir"
 LOG_DIR="log/mt_test"
-TASK_CONFIG="config/cheetah_vel/40tasks_offline.json"
+TASK_CONFIG="config/cheetah_dir/2tasks_offline.json"
 MACAW_PARAMS="config/alg/multitask.json"
 
 ./scripts/runner.sh $NAME $LOG_DIR $TASK_CONFIG $MACAW_PARAMS
