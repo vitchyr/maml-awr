0a059d6a4c8edc4bde4e734ef5035fa3f42ffe5a
jasonpeng142@hotmail.com <jasonpeng142@hotmail.com>
2020-05-31 19:24:44 -0700

    no boostrap for walker

--- scripts/multitask_walker.sh
+++ scripts/multitask_walker.sh
@@ -3,6 +3,6 @@
 NAME="multitask_walker"
 LOG_DIR="log/mt_test"
 TASK_CONFIG="config/walker_params/50tasks_offline_iris.json"
-MACAW_PARAMS="config/alg/multitask.json"
+MACAW_PARAMS="config/alg/multitask_nobootstrap.json"
 
 ./scripts/runner.sh $NAME $LOG_DIR $TASK_CONFIG $MACAW_PARAMS
