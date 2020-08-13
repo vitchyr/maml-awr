cbbea233d44ae5672d15bd00170d65880418767d
Eric Mitchell <em7@iris-ws-3.stanford.edu>
2020-08-12 22:54:58 -0700

    Added TD3 scripts

---
+++ scripts/td3_ant.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=iris
+#SBATCH --time=96:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=20G
+#SBATCH --gres=gpu:1
+#SBATCH --job-name="ant"
+
+eval "$(conda shell.bash hook)"
+conda activate macaw
+python -u -m run --device cuda:0 --name mql_ant --log_dir log/td3ctx --task_config config/ant_dir/50tasks_offline.json --macaw_param config/alg/td3ctx.json --td3ctx
